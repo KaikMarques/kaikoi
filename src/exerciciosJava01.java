d07ba7ef251d87a4ca0370b3987cdda28d84acfa
Kaik Xavier Marques de Oliveira <kaikmarques.ti@hotmail.com>
2024-08-17 16:52:47 -0300

    ExerciciosJava

---
+++ src/exerciciosJava01.java
@@ -0,0 +1,14 @@
+public class App {
+    //Imprimindo textos
+    public static void main(String[] args) {
+        // 1) Imprimir o texto "Ola mundo"
+        System.out.println("Olá mundo");
+        // 2) Imprimir o número 522
+        System.out.println(522);
+        // 3) Imprimir uma linha em branco
+        System.out.println();
+        // 4) imprimir mais um texto
+        System.out.println("Seja bem vindo a lista de exercicios resolvidos");
+    }
+    
+}
